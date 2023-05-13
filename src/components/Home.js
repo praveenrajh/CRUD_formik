5cd80585762ebca9bc19e5358fee8f4f440ca3d6
praveenrajh <ppcupcs@gmail.com>
2023-05-13 16:17:34 +0530

    Files Added

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Home.css'
+
+export const Home = () => {
+  return (
+    <div>
+      <img src='https://blog.darwinbox.com/hubfs/Blog%20Image-2.png' className='home'></img>
+    </div>
+  )
+}
